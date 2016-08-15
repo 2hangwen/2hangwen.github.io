22c404b542c41a12ed5a224147ea0a830ce0cb33
hellooozhangwen <xue0808@aliyun.com>
2016-08-15 18:56:44 +0800

    Update act_for_css.js

--- act_for_css.js
+++ act_for_css.js
@@ -39,7 +39,7 @@
        $("div#menu span div#ds").click(function(){$("div#ds-reset").toggle()});
        
        $("style").eq(0).append("div#menu span div#life{width: 100px;height: 100px;background: skyblue;border-radius: 100px;}");
-       $("div#menu span").append("<div id=life > </div> "); 
+       $("div#menu").append("<span><div id=life > </div></span> "); 
       // $("<div id=fuck_life ></div>").insertAfter("div#menu div#ds");
        $("div#menu span div#life").click(function(){$("div#log").toggle()});
    }
