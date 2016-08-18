4aa70f64ac4a1a2f492937b9711fb2f00416cedb
hellooozhangwen <xue0808@aliyun.com>
2016-08-18 12:58:16 +0800

    Update link_url.js

--- link_url.js
+++ link_url.js
@@ -1,8 +1,8 @@
 function create_link_bycss(){
        $("style").eq(0).append("div#menu span div#link_url{float: left;width: 100px;height: 100px;background: deepskyblue;border-radius: 100px;}");
        $("div#menu").append("<span> <div id=link_url ><div style=text-align:center;line-height:100px >好友链接</div> </div></span>");
-       var link="<a href='http://www.pandablessing.com' >pandablessing</a>";
-       $("div#menu span div#link_url").click(function(){show(link,50000)});
+       var link="<a href='http://www.pandablessing.com' target='view_window' >wfq_best</a>";
+       $("div#menu span div#link_url").click(function(){show(link,10000)});
       // $("div#menu span div#link_url").append(link);
 }
 
