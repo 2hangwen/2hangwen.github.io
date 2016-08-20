2815980583cd8a377726ad2e925c31b1b9bbbdc1
hellooozhangwen <xue0808@aliyun.com>
2016-08-20 13:07:39 +0800

    Update read_somelog.js

--- read_somelog.js
+++ read_somelog.js
@@ -3,18 +3,18 @@
             var key = "iframe#" + t;
             var tit="";
             var img="";
-            if(!$(key).contents().find("title").prop("text").indexOf("not")){
+            if(! $(key).contents().find("title").prop("text").indexOf("not")){
              //获取iframe的document,查找title标签并获取内容，这里用了？：;	
               var tit = $(key).contents().find("title").length ? $(key).contents().find("title").prop("text") : false;
               var img = $(key).contents().find("img").length ? $(key).contents().find("img").eq(0).prop("src") : false;
               console.log(img);
-              if (tit){
+             // if (tit){
               // alert(tit);
                add_log($(key).prop("src"),tit);
                //清空iframe里的数据
                //$(key).prop("src","");
               // $(key).remove();
-              }
+            //  }
             } else{
                 $(key).remove();
               }
