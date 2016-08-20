3251387f3311dc1691cf481d43dd2678e0b08174
hellooozhangwen <xue0808@aliyun.com>
2016-08-20 12:35:29 +0800

    Update read_somelog.js

--- read_somelog.js
+++ read_somelog.js
@@ -22,7 +22,7 @@
 function read_log(){
   for(i=1;i<30;++i){
      var filename=i+".html";
-     var tag="<iframe id="+filename+" src=some_log/"+filename+" onload=get_iframe(this.id) ><iframe>";
+     var tag="<iframe id=loghtml src=some_log/"+filename+" onload=get_iframe(this.id) ><iframe>";
      console.log(tag);
     // $("body").append(tag);
      $(tag).insertBefore("iframe#music163");
