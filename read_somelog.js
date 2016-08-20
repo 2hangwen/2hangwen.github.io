501d2432385818621591e2c196c07608f7b32d5c
hellooozhangwen <xue0808@aliyun.com>
2016-08-20 12:59:31 +0800

    Update read_somelog.js

--- read_somelog.js
+++ read_somelog.js
@@ -23,7 +23,7 @@
 
 function read_log(){
   for(i=1;i<30;++i){
-     var filename=i+".html";
+     var filename=i+"html";
      var tag="<iframe id="+filename+" src=some_log/"+filename+" onload=get_iframe(this.id) ><iframe>";
      console.log(tag);
     // $("body").append(tag);
