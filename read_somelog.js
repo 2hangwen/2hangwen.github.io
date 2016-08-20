b9efd84eadcd310113700e5c5ce83deccfb1e384
hellooozhangwen <xue0808@aliyun.com>
2016-08-20 12:15:25 +0800

    Update read_somelog.js
    添加读取iframe的过程

--- read_somelog.js
+++ read_somelog.js
@@ -1,8 +1,28 @@
+  //获取加载了iframe里的内容  
+  function get_iframe(t) {
+            var key = "iframe#" + t;
+            var tit="";
+            var img="";
+            if($(key).length){
+             //获取iframe的document,查找title标签并获取内容，这里用了？：;	
+              var tit = $(key).contents().find("title").length ? $(key).contents().find("title").prop("text") : false;
+              var img = $(key).contents().find("image").length ? $(key).contents().find("image").eq(0).prop("src") : false;
+              console.log(img);
+              if (tit && img){
+              // alert(tit);
+               add_log($(key).prop("src"),tit);
+               //清空iframe里的数据
+               //$(key).prop("src","");
+               $(key).remove();
+              } 
+            }
+  }
+
 
 function read_log(){
   for(i=1;i<30;++i){
      var filename=i+".html";
-     var tag="<iframe id="+filename+" src=some_log/"+filename+" onload=get_iframe_title(this.id) ><iframe>";
+     var tag="<iframe id="+filename+" src=some_log/"+filename+" onload=get_iframe(this.id) ><iframe>";
      console.log(tag);
     // $("body").append(tag);
      $(tag).insertBefore("iframe#music163");
